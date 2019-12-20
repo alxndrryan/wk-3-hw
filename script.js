58bbbd5c2080ff57fe8005eb454a6d4dea006bec
Rand al'Thor <alexanderhogle@gmail.com>
2019-12-19 21:53:53 -0600

    adds function to get random lowercase letter

---
+++ script.js
@@ -0,0 +1,7 @@
+// Generator Functions - http://www.net-comber.com/charset.html
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+console.log(getRandomLower());
